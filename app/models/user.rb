8e8827e6713e7ecc79b54c0628f7d777a88f9cdc
fukitayujiro <hujiiryotaku@gmail.com>
2020-08-14 15:46:58 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,28 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :birth_date, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birth_date
+  end
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+  with_options presence: true, uniqueness: true do
+    validates :email
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  with_options presence: true, confirmation: true, length: { minimum: 6 },format: { with: PASSWORD_REGEX } do
+    validates :password
+  end
+
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+  with_options presence: true, format: { with: VALID_NAME_REGEX, message: '全角文字を使用してください' } do
     validates :first_name
     validates :last_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナ文字を使用してください' } do
+  VALID_KANA_REGEX = /\A[ァ-ン]+\z/
+  with_options presence: true, format: { with: VALID_KANA_REGEX, message: '全角カタカナ文字を使用してください' } do
     validates :first_name_kana
     validates :last_name_kana
   end
