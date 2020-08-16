9dd0c1a7ada4b0d30aa538405dd32099483bf68e
fukitayujiro <hujiiryotaku@gmail.com>
2020-08-16 12:51:05 +0900

    itemテーブルにuser_idとimageが保存されるようにするため、paramsとアソシエーション の記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,9 @@ class ItemsController < ApplicationController
   def index
   end
 
+  private
+  def item_params
+    params.require(:item).permit(:name, :explain, :price, :image, :category_id, :item_status_id, :sending_charge_id, :sending_region_id, :sending_day_id,).merge(user_id: current_user.id)
+  end
+  
 end
