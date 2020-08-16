2b4f1e4e3f1751a87d7374e4f341c2fc55dd160f
fukitayujiro <hujiiryotaku@gmail.com>
2020-08-16 13:33:22 +0900

    ログインユーザーのみ購入画面へ遷移できる。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: :index
 
   def index
   end
@@ -7,5 +8,11 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :explain, :price, :image, :category_id, :item_status_id, :sending_charge_id, :sending_region_id, :sending_day_id,).merge(user_id: current_user.id)
   end
-  
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
 end
