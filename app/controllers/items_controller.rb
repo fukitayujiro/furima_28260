01626ce81058788d84ffa776f859afa1c9b122df
fukitayujiro <hujiiryotaku@gmail.com>
2020-08-19 17:40:20 +0900

    不要なメソッドを削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,9 +17,6 @@ class ItemsController < ApplicationController
     end
   end
 
-  def itemprice
-  end
-
   private
   def item_params
     params.require(:item).permit(:name, :explain, :price, :image, :category_id, :item_status_id, :sending_charge_id, :sending_region_id, :sending_day_id,).merge(user_id: current_user.id)
