d1df255b5951deb9b0aa6f9122bb96be32051277
fukitayujiro <hujiiryotaku@gmail.com>
2020-08-21 16:41:45 +0900

    詳細ページから購入画面へ遷移するまで
    transactionsコントローラー作成
    itemsコントローラーにネストする形でルーティング
    詳細画面から購入ボタンクリックでページ遷移

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: "items#index"
   devise_for :users
-  resources :items
+  resources :items do
+    resources :transactions
+  end
   get 'items/new', to: 'items#itemprice'
 end
