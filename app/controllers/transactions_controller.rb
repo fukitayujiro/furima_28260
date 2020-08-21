42739ba1fe0202ed1b73bac3dc2dcdbdbaffaae3
fukitayujiro <hujiiryotaku@gmail.com>
2020-08-21 18:56:14 +0900

    未ログインユーザーはログイン画面、出品者はトップへ遷移

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,7 +1,23 @@
 class TransactionsController < ApplicationController
+  before_action :move_to_index, only: :index
+  before_action :move_to_login, only: :index
 
   def index
     @item = Item.find(params[:item_id])
+  end
+
+  private
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    if user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
+  def move_to_login
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
   
 end
