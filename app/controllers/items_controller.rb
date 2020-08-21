1c6ac7952e6f057ba046bfbc2477cc1f9039a9c9
fukitayujiro <hujiiryotaku@gmail.com>
2020-08-21 13:05:20 +0900

    出品者以外は商品編集ページへ遷移できない

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to new_user_session_path
+    end
   end
 
   def update
