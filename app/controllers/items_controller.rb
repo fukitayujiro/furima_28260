23bb16c03dba51a49efb51f5cd5199a4678ebb4b
fukitayujiro <hujiiryotaku@gmail.com>
2020-08-25 18:34:13 +0900

    一覧表示昇順に変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :find_item_id, only: [:edit, :show]
 
   def index
-    @items = Item.all
+    @items = Item.all.order("created_at DESC")
   end
 
   def new
